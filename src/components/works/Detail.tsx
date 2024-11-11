bc99ee082694286627d1d44effa3f768c2cdf89f
hyemi <gpal4434@naver.com>
2024-11-11 21:53:08 +0900

    details 디자인 시작

--- src/components/works/Detail.tsx
+++ src/components/works/Detail.tsx
@@ -1,14 +1,58 @@
+"use client";
+
 import { prompt } from "@/app/fonts";
 import { DataValueProps } from "@/lib/data";
 import Image from "next/image";
 import ClickMe from "../main/ClickMe";
+import { useRouter } from "next/navigation";
+import { IoIosArrowBack, IoIosArrowForward, IoMdHome } from "react-icons/io";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import SwiperCore, { Navigation, Pagination } from "swiper";
+SwiperCore.use([Navigation, Pagination]);
 
 const Detail = ({ data }: DataValueProps) => {
-    console.log(data);
+    console.log("ddd", data);
+    const router = useRouter();
+
     return (
         <>
-            <div className="mx-auto w-[1280px] p-28">
-                <div className="flex gap-16">
+            <div className="mx-auto w-[1280px] p-28 pt-10">
+                <div className="flex justify-between border-b border-solid border-text">
+                    <button className="group flex items-center gap-2 p-5 pl-0" onClick={() => router.push("/")}>
+                        <IoIosArrowBack size={18} />
+                        <div className="flex items-center gap-1">
+                            <IoMdHome color="#666" size={18} />
+                            <span className="group-hover:underline">메인으로</span>
+                        </div>
+                    </button>
+                    <div className="flex items-center gap-5">
+                        {data && data.key && data.key !== 1 && (
+                            <button
+                                className="group flex items-center gap-2"
+                                onClick={() => router.push(`/works/${data.key - 1}`)}
+                            >
+                                <IoIosArrowBack size={18} />
+                                <span className="group-hover:underline">이전 글</span>
+                            </button>
+                        )}
+                        {data && data.key && data.key !== 1 && data.key < 8 && <span>·</span>}
+                        {data && data.key && data.key < 8 && (
+                            <button
+                                className="group flex items-center gap-2"
+                                onClick={() => router.push(`/works/${data.key + 1}`)}
+                            >
+                                <span className="group-hover:underline">다음 글</span>
+                                <IoIosArrowForward size={18} />
+                            </button>
+                        )}
+                    </div>
+                </div>
+                <div className="flex flex-col gap-1 pt-10">
+                    <h5 className="text-xl text-gray-500">Project Details</h5>
                     <div className="relative flex h-72 w-96 justify-center rounded-xl border">
                         <Image
                             src={`/images/thumb/${data.src}.png`}
@@ -18,8 +62,7 @@ const Detail = ({ data }: DataValueProps) => {
                             alt={`work_thumb_${data.key}`}
                         />
                     </div>
-                    <div className={`flex flex-col gap-2 ${prompt.className}`}>
-                        <h5 className="text-gray-400">Project Details</h5>
+                    <div className={`flex flex-col gap-3 pt-5 ${prompt.className}`}>
                         <h3
                             className={`text-6xl font-bold text-text transition-all delay-100 duration-200 group-hover:text-white`}
                         >
@@ -37,6 +80,29 @@ const Detail = ({ data }: DataValueProps) => {
                         </div>
                     </div>
                 </div>
+                {data.pc && (
+                    <Swiper
+                        loop={true}
+                        spaceBetween={50}
+                        // pagination={{ clickable: true }}
+                        slidesPerView={1}
+                        // navigation={true}
+                    >
+                        {data.pc.map((v, i) => (
+                            <SwiperSlide key={i}>
+                                <div className="relative aspect-[16/9] max-h-[90vh] w-full rounded-xl border border-solid border-border">
+                                    <Image
+                                        src={`/images/details/${v}.png`}
+                                        layout="fill"
+                                        objectFit="contain"
+                                        alt={`img-${v.i}`}
+                                        className="rounded-xl"
+                                    />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                )}
             </div>
             <ClickMe />
         </>
--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -5,6 +5,8 @@ export interface DataValueProps {
     skill: string[];
     desc: string;
     date: string;
+    pc?: string[];
+    mobile?: string[];
 }
 export const data: DataValueProps[] = [
     {
@@ -14,11 +16,13 @@ export const data: DataValueProps[] = [
         skill: ["HTML", "CSS"],
         desc: "Vue2.0 로 제작된 승부예측 베팅 사이트로 기존 운영되고 있던 사이트를 유지보수 및 추가 기능 퍼블리싱 100% 참여함",
         date: "2022.06 ~",
+        pc: ["spolive01", "spolive02", "spolive03", "spolive04", "spolive05"],
+        mobile: ["spolivem01", "spolivem02", "spolivem03"],
     },
 
     {
         src: "next",
-        key: 1234,
+        key: 2,
         title: "SPOLIVE Renewal",
         skill: ["NEXT.js", "Vanilla Extract", "Typescript"],
         desc: "Vue2.0으로 제작된 기존 스포라이브를 next.js로 리뉴얼하면서, 디자인 개선하여 좀 더 친근하고 편한 UI/UX를 만들었으며 본격적으로 프론트엔드 개발에 참여함",
@@ -27,7 +31,7 @@ export const data: DataValueProps[] = [
 
     {
         src: "global",
-        key: 124,
+        key: 3,
         title: "BETNEX",
         skill: ["NEXT.js", "Vanilla Extract", "React.js", "Typescript"],
         desc: "VUE2.js로 제작된 기존 스포라이브를 next.js로 바꿈",
@@ -35,7 +39,7 @@ export const data: DataValueProps[] = [
     },
     {
         src: "gift",
-        key: 114,
+        key: 4,
         title: "FURSYS MORE",
         skill: ["JavaScript", "HTML", "CSS"],
         desc: "SIDE PROJECT로 진행했던 상품권 매입 사이트로 Vanilla Javascript로 이벤트 처리, 모달 기능 구현 등 퍼블리싱 100% 참여함",
@@ -43,7 +47,7 @@ export const data: DataValueProps[] = [
     },
     {
         src: "native",
-        key: 2442,
+        key: 5,
         title: "SPOLIVE App",
         skill: ["React Native", "Vanilla Extract"],
         desc: "기본 모바일 웹이었던 스포라이브를 React Native로 App 구현하는데 UI 개발 100%와 페이지 개발 30% 기여함",
@@ -51,26 +55,30 @@ export const data: DataValueProps[] = [
     },
     {
         src: "china",
-        key: 684,
+        key: 6,
         title: "Let's Go China",
         skill: ["HTML", "CSS", "jQeury"],
         desc: "순수교육의 렛츠고 시리즈 중 하나로 새로 런칭한 교육 서비스로 퍼블리싱 100% 참여함",
         date: "2020.09 ~ 2020.10",
+        pc: ["china"],
     },
     {
         src: "admin",
-        key: 743,
+        key: 7,
         title: "VZON Admin",
         skill: ["HTML", "CSS", "jQeury"],
         desc: "밴대리점 통합 시스템 VZON 솔루션의 어드민 페이지 퍼블리싱 100% 참여함",
         date: "2019.07 ~ 2019.09",
+        pc: ["admin01", "admin02", "admin03", "admin04"],
     },
     {
-        src: "VZON",
-        key: 732,
+        src: "vzon",
+        key: 8,
         title: "VZON",
         skill: ["HTML", "CSS", "jQeury"],
         desc: "밴대리점 통합 시스템 VZON 솔루션 PC와 MOBILE 퍼블리싱 100% 참여함",
         date: "2018.05 ~ 2019.12",
+        pc: ["vzon01", "vzon02", "vzon03", "vzon04", "vzon05"],
+        mobile: ["vzonm01", "vzonm02", "vzon,03", "vzonm04"],
     },
 ];
