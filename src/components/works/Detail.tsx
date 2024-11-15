2adf69b7b34d4073e74f1ca33622093df37e2b6e
hyemi <hmkim@spolive.comdfd>
2024-11-15 14:06:59 +0900

    swiper 수정

--- src/components/works/Detail.tsx
+++ src/components/works/Detail.tsx
@@ -3,25 +3,41 @@
 import { prompt } from "@/app/fonts";
 import { DataValueProps } from "@/lib/data";
 import Image from "next/image";
-import ClickMe from "../main/ClickMe";
+import ClickMe from "../common/ClickMe";
 import { useRouter } from "next/navigation";
 import { IoIosArrowBack, IoIosArrowForward, IoMdHome } from "react-icons/io";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Scrollbar } from "swiper/modules";
+import SwiperCore from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import SwiperCore, { Navigation, Pagination } from "swiper";
-SwiperCore.use([Navigation, Pagination]);
+import { useEffect, useState } from "react";
+import RailText from "../common/RailText";
 
 const Detail = ({ data }: DataValueProps) => {
     console.log("ddd", data);
+    SwiperCore.use([Navigation, Scrollbar]);
     const router = useRouter();
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 720);
+        };
+        handleResize();
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     return (
         <>
-            <div className="mx-auto w-[1280px] p-28 pt-10">
-                <div className="flex justify-between border-b border-solid border-text">
+            <div className="p-28 pt-10 xl:p-10">
+                <div className="mx-auto flex w-[1280px] justify-between border-b border-solid border-text">
                     <button className="group flex items-center gap-2 p-5 pl-0" onClick={() => router.push("/")}>
                         <IoIosArrowBack size={18} />
                         <div className="flex items-center gap-1">
@@ -51,7 +67,7 @@ const Detail = ({ data }: DataValueProps) => {
                         )}
                     </div>
                 </div>
-                <div className="flex flex-col gap-1 pt-10">
+                <div className="mx-auto flex w-[1280px] flex-col gap-1 pt-10">
                     <h5 className="text-xl text-gray-500">Project Details</h5>
                     <div className="relative flex h-72 w-96 justify-center rounded-xl border">
                         <Image
@@ -69,7 +85,9 @@ const Detail = ({ data }: DataValueProps) => {
                             {data.title}
                         </h3>
 
-                        <h5 className="transition-all delay-100 duration-200 group-hover:text-white">{data.desc}</h5>
+                        <h5 className="text-lg transition-all delay-100 duration-200 group-hover:text-white">
+                            {data.desc}
+                        </h5>
 
                         <div className="flex flex-wrap gap-1">
                             {data.skill.map((v, i) => (
@@ -80,31 +98,44 @@ const Detail = ({ data }: DataValueProps) => {
                         </div>
                     </div>
                 </div>
-                {data.pc && (
-                    <Swiper
-                        loop={true}
-                        spaceBetween={50}
-                        // pagination={{ clickable: true }}
-                        slidesPerView={1}
-                        // navigation={true}
-                    >
-                        {data.pc.map((v, i) => (
-                            <SwiperSlide key={i}>
-                                <div className="relative aspect-[16/9] max-h-[90vh] w-full rounded-xl border border-solid border-border">
-                                    <Image
-                                        src={`/images/details/${v}.png`}
-                                        layout="fill"
-                                        objectFit="contain"
-                                        alt={`img-${v.i}`}
-                                        className="rounded-xl"
-                                    />
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                )}
+                <div className="pt12 pb-16">
+                    {data.pc && (
+                        <Swiper
+                            loop={true}
+                            spaceBetween={0}
+                            // pagination={{ clickable: true }}
+                            slidesPerView={1}
+                            navigation={isMobile ? false : true}
+                            breakpoints={{
+                                720: {
+                                    slidesPerView: 1.5,
+                                    centeredSlides: true,
+                                    spaceBetween: 30,
+                                    pagination: true,
+                                },
+                            }}
+                        >
+                            {data.pc.map((v, i) => (
+                                <SwiperSlide key={i}>
+                                    <div className="relative aspect-[16/9] max-h-[58vh] w-full rounded-xl bg-white shadow-xl">
+                                        <Image
+                                            src={`/images/details/${v}.png`}
+                                            layout="fill"
+                                            fill
+                                            objectFit="contain"
+                                            alt={`img-${v.i}`}
+                                            className="rounded-xl"
+                                        />
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    )}
+                </div>
+                <div className="columns-2 text-lg font-normal leading-relaxed text-gray-800">{data.details}</div>
             </div>
             <ClickMe />
+            <RailText />
         </>
     );
 };
