d9f96a460dd90f06d7c6e3b6726bceca0c93511f
hyemi <hmkim@spolive.comdfd>
2024-11-22 20:11:40 +0900

    meta data 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,19 @@ import "@/app/globals.css";
 
 export const metadata: Metadata = {
     title: "HEMPACK ::: 웹 퍼블리셔 김혜미",
-    description: "웹 퍼블리셔 지원자 김혜미 포트폴리오",
+    siteName: "HEMPACK | 혜미패키지",
+    description: "웹 퍼블리셔(경력) 지원자 김혜미 포트폴리오입니다.",
+    keywords: [
+        "웹 퍼블리셔 포트폴리오",
+        "퍼블리셔 포트폴리오",
+        "경력 퍼블리셔",
+        "프론트엔드 개발자 포트폴리오",
+        "프론트엔드 개발자",
+        "UI 개발자",
+        "UI UX 개발자 포트폴리오",
+    ],
+    url: "https://hempack.vercel.app/",
+    ogImage: "/public/images/ogImage.png",
 };
 
 export const viewport: Viewport = {
