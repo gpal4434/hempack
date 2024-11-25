130c9a16da7ce37a832b4c34b68c61173f7e23b0
hyemi <hmkim@spolive.comdfd>
2024-11-25 11:26:29 +0900

    안쓰는 useState 삭제

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import { prompt } from "@/app/fonts";
 import ClickMe from "../common/ClickMe";
